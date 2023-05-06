d22b84619fc7d24f8bd9da9a423c835a174895ba
Wendina <wendina.teng@gmail.com>
2023-05-06 19:48:46 +0000

    change config.file_watcher becaus to adapt to vagrant machine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,8 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker <-- doesn't work because it's running on vagrant, rails cannot detect changes to your file on the local machine with the file watcher
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
