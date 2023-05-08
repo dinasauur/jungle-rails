15d0b0adf5b8bed12c30ee67f6981ba95d010642
Wendina <wendina.teng@gmail.com>
2023-05-08 19:21:07 +0000

    modify validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, :last_name, :password, presence: true
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false}
-  validates :password_must_equal_password_confirmation
   validates_confirmation_of :password
   validates :password, presence: true, length: { minimum: 6 }
 end
