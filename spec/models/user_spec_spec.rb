95bd9f85aec41513d05fc20bcda6024de2e9e8d7
Wendina <wendina.teng@gmail.com>
2023-05-08 19:21:58 +0000

    add first validation test to check if user with all attribnutes preseutes present is valid

--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe 'Validations' do
+    it "is valid with a name, password, email" do
+      user = User.new(
+        name: 'Lula Lounge',
+        email: 'lula@lounge.com',
+        password: '123456'
+      )
+
+      expect(user).to be_valid
+    end
+
+    
+  end
 end
