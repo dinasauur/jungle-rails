59d7bdda3403cd1334e1d06761a914ba764d59f9
Wendina <wendina.teng@gmail.com>
2023-05-08 19:02:58 +0000

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, :last_name, :password, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
+  validates :password_must_equal_password_confirmation
+  validates_confirmation_of :password
+  validates :password, presence: true
 end
