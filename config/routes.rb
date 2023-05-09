b43ff6c67b858e24ed24f81642fbcd13732375b9
Wendina <wendina.teng@gmail.com>
2023-05-09 01:25:11 -0400

    remove index from users route as it is not needed

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
-  resources :users, only: [:index, :create, :new]
+  resources :users, only: [:create, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
