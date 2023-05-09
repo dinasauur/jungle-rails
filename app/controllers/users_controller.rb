f05dce2754426661e6ed938aaca3ac2b72fc8670
Wendina <wendina.teng@gmail.com>
2023-05-09 01:49:28 -0400

    add has secure password to user model and make minor tweak to syntax in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
    @user = User.new(user_params)
 
    if @user.save
-    session[:user_id] = user.id
+    session[:user_id] = @user.id
     redirect_to root_path
    else
     redirect_to new_user_path
+   end
   end
 
   private
