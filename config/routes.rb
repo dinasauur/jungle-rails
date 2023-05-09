af5614a5468271b642c339404beedf00565c35ad
Wendina <wendina.teng@gmail.com>
2023-05-09 01:51:52 -0400

    add actions to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   resources :about, only: [:index]
 
   resources :users, only: [:create, :new]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
