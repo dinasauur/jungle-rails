6bb44d0fa03f3ef2d68f70c2295d5eaef8fa4f25
Wendina <wendina.teng@gmail.com>
2023-05-08 22:40:44 -0400

    add user to routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
+  resources :users, only: [:index, :create, :new]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
