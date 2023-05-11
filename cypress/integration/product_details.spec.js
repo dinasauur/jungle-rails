a41e938cb3525b43c40d60d5d389e67bc293a14f
Wendina <wendina.teng@gmail.com>
2023-05-11 21:14:41 +0000

    modify file to add more tests

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -4,6 +4,10 @@ describe('product-show-page', () => {
   beforeEach(() => {
     cy.visit('/')
   })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
   
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
@@ -12,6 +16,6 @@ describe('product-show-page', () => {
   it("should open up a a show view of that product", () => {
     cy.get(".products article").first().click();
 
-    // cy.contains('.products-show article', '23');
+    cy.contains('.product-detail', 'Scented Blade');
   })
 })
